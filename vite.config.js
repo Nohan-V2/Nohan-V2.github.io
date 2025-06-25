de98d2ade320003af8cf1aa320030110319fd2a5
Nohan-V2 <nseck@edenschool.fr>
2025-06-25 15:09:09 +0200

    Update GitHub Actions workflow and Vite base path
    Refactored the GitHub Actions workflow to use separate build and deploy jobs, updated to use official actions for artifact upload and deployment, and set permissions explicitly. Changed Vite config base path from '/Nohan-V2.github.io/' to '/'.

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Nohan-V2.github.io/', // Remplace par le nom de ton repo GitHub
+  base: '/',
 });
